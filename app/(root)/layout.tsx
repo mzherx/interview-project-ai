ca442c2b77b03e90c1daf74afb242349f3705214
Mazher <mazheriftikhar1@gmail.com>
2025-04-15 15:02:35 +0500

    15-Apr

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+const layout = ({ children } : { children: ReactNode}) => {
+  return (
+    <div>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default layout
